b90f934acbc729710715d61bdbd98ed6fed5038a
Joel <58995538+jtpotato@users.noreply.github.com>
2023-01-30 20:44:51 +1100

    fix a bit of a stupid bug

--- commands/ocr.ts
+++ commands/ocr.ts
@@ -5,7 +5,7 @@ export const ocr: BuerCommand = {
   data: new SlashCommandBuilder()
     .setName("ocr")
     .setDescription(
-      "Buer uses All Schemes To Know, and reads text from the most recent image that you sent. This ideally should not be necessary, as Buer will watch for images in Watch mode."
+      "Buer uses All Schemes To Know, and reads text from the most recent image that you sent."
     ),
   async execute(interaction: CommandInteraction) {
     await interaction.reply("Pong!");
