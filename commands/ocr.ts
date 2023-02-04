d7dfc23dfdbfd7e59a9953f7ec5c87f573f45700
Joel <58995538+jtpotato@users.noreply.github.com>
2023-02-04 13:32:52 +1100

    finish ocr?

--- commands/ocr.ts
+++ commands/ocr.ts
@@ -1,5 +1,11 @@
-import { SlashCommandBuilder, CommandInteraction } from "discord.js";
-import { BuerCommand } from "../types/TypeBuerCommand"
+import {
+  SlashCommandBuilder,
+  CommandInteraction,
+  Collection,
+  Message,
+} from "discord.js";
+import { BuerCommand } from "../types/TypeBuerCommand";
+import Tesseract from "tesseract.js"
 
 export const ocr: BuerCommand = {
   data: new SlashCommandBuilder()
@@ -8,6 +14,28 @@ export const ocr: BuerCommand = {
       "Buer uses All Schemes To Know, and reads text from the most recent image that you sent."
     ),
   async execute(interaction: CommandInteraction) {
-    await interaction.reply("Pong!");
+    await interaction.deferReply();
+
+    let imageURL = "Oops, probably can't find the image or something";
+    let messages = await interaction.channel?.messages.fetch();
+
+    let filteredMessages = messages?.filter(
+      (message) => message.attachments.size > 0
+    );
+
+    let imageMessage = filteredMessages
+      ?.sort((a, b) => b.createdTimestamp - a.createdTimestamp)
+      .first();
+
+    if (imageMessage?.attachments) {
+      let attachments = imageMessage?.attachments;
+      imageURL = attachments.first()?.url!;
+
+      let data = await Tesseract.recognize(imageURL, 'eng')
+
+      await interaction.editReply(data.data.text);
+    } else {
+      await interaction.editReply("No attachments found, or something");
+    }
   },
 };
